172a1c7421aebb33c15c27d42126873e48633ab1
Alberto <alberto.alvarez.sanz@gmail.com>
2020-11-07 21:38:35 +0100

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -6,7 +6,44 @@ fun main() {
 
 
     // Haz un ejercicio como output tenga:
+
+    var contadorPerro = 0
+    var contadorGato = 0
+    var contador = 0
+    var mediaGato = 0
+    var mediaPerros = 0
+    var mediaPersonas = 0
+
+
+
     // El peso medio de los gatos es:
+    lista.forEach{
+        if (it is Gato) {
+            mediaGato += it.peso
+            contadorGato +1
+
+        }
+    }
+
     // El peso medio de los perros es:
+    lista.forEach{
+        if (it is Perro) {
+            mediaPerros += it.peso
+            contadorPerro +1
+        }
+    }
     // El peso medio de las personas es:
+    lista.forEach{
+        if (it is Persona) {
+            mediaPersonas += it.peso
+            contador +1
+        }
+    }
+
+    println("El peso medio de los gatos es de "+ mediaGato/3 )
+    println("El peso medio de los perros es de "+ mediaPerros/3 )
+    println("El peso medio de los personas es de "+ mediaPersonas/3 )
+
+
+
 }
