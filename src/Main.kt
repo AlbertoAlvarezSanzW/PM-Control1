48606f36513feb55fda7e099cd8ebb0c9fd4d1ff
Alberto <alberto.alvarez.sanz@gmail.com>
2020-11-07 21:54:19 +0100

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -6,43 +6,53 @@ fun main() {
 
 
     // Haz un ejercicio como output tenga:
+    // El peso medio de los gatos es:
+    // El peso medio de los perros es:
+    // El peso medio de las personas es:
+
+
 
     var contadorPerro = 0
+    var pesoPerro = 0
+
     var contadorGato = 0
-    var contador = 0
-    var mediaGato = 0
-    var mediaPerros = 0
-    var mediaPersonas = 0
+    var pesoGato = 0
+
+    var contadorPersona = 0
+    var pesoPersona = 0
 
 
+    //Bucle para recorrer la Lista
+    lista.forEach {
 
-    // El peso medio de los gatos es:
-    lista.forEach{
-        if (it is Gato) {
-            mediaGato += it.peso
-            contadorGato +1
+        when(it){
 
+            is Perro -> {
+                contadorPerro++
+                pesoPerro += it.peso
+            }
+
+            is Gato -> {
+                contadorGato++
+                pesoGato += it.peso
+            }
+
+            is Persona -> {
+                contadorPersona++
+                pesoPersona += it.peso
+            }
         }
     }
+    // es importante seguir el orden peso / contador
+    println("El peso medio de los gatos es:${pesoGato/contadorGato}")
+    println("El peso medio de los perros es:${pesoPerro/contadorPerro}")
+    println("El peso medio de las personas es:${pesoPersona/contadorPersona}")
 
-    // El peso medio de los perros es:
-    lista.forEach{
-        if (it is Perro) {
-            mediaPerros += it.peso
-            contadorPerro +1
-        }
-    }
-    // El peso medio de las personas es:
-    lista.forEach{
-        if (it is Persona) {
-            mediaPersonas += it.peso
-            contador +1
-        }
-    }
 
-    println("El peso medio de los gatos es de "+ mediaGato/3 )
-    println("El peso medio de los perros es de "+ mediaPerros/3 )
-    println("El peso medio de los personas es de "+ mediaPersonas/3 )
+
+
+
+
 
 
 
